786b010b9d548ef0f91a8b5b995200576576d601
JasonCheungDev <jayson.h.cheung@gmail.com>
2020-02-09 13:18:51 -0800

    Fix plex auto inviter with preferred server page

--- plex-auto-invite.js
+++ plex-auto-invite.js
@@ -4,7 +4,7 @@ const SETTINGS = require('./anilist-downloader-settings.js')
 
 async function inviteUsers(emails) {
     const browser = await puppeteer.launch({
-        headless: false,
+        headless: true,
         slowMo: 50,
         defaultViewport: null
     });
@@ -12,18 +12,35 @@ async function inviteUsers(emails) {
     await page.goto('http://127.0.0.1:32400/');
 
     // LOGIN & NAVIGATE TO INVITE PAGE
-    const signInButtonSelector = 'button[data-qa-id="signIn--email"]'
+    console.log("Login screen")
+    const signInButtonSelector = 'button[data-qa-id="signIn--email"]' // cannot wait for not disabled
     await page.waitForSelector(signInButtonSelector)
-    await page.waitFor(1000);
+    await page.waitFor(2000)
     await page.click(signInButtonSelector)
 
     await page.type('#email', SETTINGS.PLEX.EMAIL)
     await page.type('#password', SETTINGS.PLEX.PASSWORD)
     await page.keyboard.press('Enter')
 
+    console.log("Logging in...")
     const accButSelector = 'button[class^="NavBar-accountButton"]'
     await page.waitForSelector(accButSelector)
-    await page.waitFor(1000)
+    await page.waitFor(2000)
+
+    // PREFERRED SERVER PAGE?
+    const preferredServerButtonSelector = 'button[class^="FirstRunExperienceStep-stepButton"]'
+    const preferredServerButton = await page.$(preferredServerButtonSelector)
+    if (preferredServerButton) {
+        console.log("Preferred server screen detected")
+        await page.click(preferredServerButtonSelector)
+        await page.waitFor(1000)
+        console.log("Finding finish button")
+        await page.waitForXPath(`//button[contains(., "Finish Setup")]`)
+        const [finishButton] = await page.$x(`//button[contains(., "Finish Setup")]`)
+        await finishButton.click()
+        await page.waitFor(1000)
+    }
+
     await page.click(accButSelector)
 
     // sketchy
@@ -31,20 +48,23 @@ async function inviteUsers(emails) {
     await page.click(userAndSharingSelector)
 
     // INVITE A USER
+    console.log("Sharing & Invite screen")
     const sendInviteEmail = async function(email) {
-        const TARGET_EMAIL = email
+        console.log(`Inviting ${email}`)
         
         // hyper sketchy
-        const shareLibrariesSelector = 'div[class^="SettingsPage-content"]  > div[class^="UsersSettingsPageListHeader"] button[class^="UsersSettingsPage"]'
+        const shareLibrariesSelector = 'div[class^="SettingsPage-content"] > div[class^="UsersSettingsPageListHeader"] button[class^="UsersSettingsPage"]'
         await page.waitForSelector(shareLibrariesSelector)
         await page.click(shareLibrariesSelector)
 
-        await page.type('#username', TARGET_EMAIL)
-        await page.waitFor(2000)
+        await page.type('#username', email)
+        await page.waitForSelector(`button[type="submit"]:not([disabled])`) // wait for Plex
         await page.keyboard.press('Enter')
     
         await page.waitFor(2000)
     
+        console.log("Desired library " + SETTINGS.PLEX.LIBRARY_NAME)
+        await page.waitForXPath(`//label[contains(., "${SETTINGS.PLEX.LIBRARY_NAME}")]//input`)
         const [checkbox] = await page.$x(`//label[contains(., "${SETTINGS.PLEX.LIBRARY_NAME}")]//input`)
         if (checkbox) {
             await checkbox.click()
