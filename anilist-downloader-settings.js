047e3c0ef797eac34c68c1ff59835f8a230c96a5
JasonCheungDev <jayson.h.cheung@gmail.com>
2020-02-09 13:28:50 -0800

    updated settings

--- anilist-downloader-settings.js
+++ anilist-downloader-settings.js
@@ -1,8 +1,19 @@
-module.export = {
+module.exports = {
+    SHARED: {
+        DELAY: 10000, 
+    },
     QBT: {
         PORT: 8080,
         USERNAME: "admin",
         PASSWORD: "password",
         DOWNLOAD_LOCATION: "C:/Anime/"
+    },
+    PLEX: {
+        EMAIL: "plex@gmail.com",
+        PASSWORD: "password",
+        LIBRARY_NAME: "LibraryName" // case sensitive
+    },
+    WEB: {
+        TITLE: "[UNOFFICIAL] AniList Plex Media Server"
     }
 }
